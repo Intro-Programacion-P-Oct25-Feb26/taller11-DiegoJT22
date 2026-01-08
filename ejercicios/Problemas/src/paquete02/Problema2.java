1a93005445e1157453da0b2370e736f8b60dd5f1
DiegoJT22 <daigoestrlar22@gmail.com>
2026-01-08 11:40:48 -0500

    solución Problema 2 - versión 1

--- ejercicios/Problemas/src/paquete02/Problema2.java
+++ ejercicios/Problemas/src/paquete02/Problema2.java
@@ -15,7 +15,20 @@ public class Problema2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] viviendas = new int[10];
+        int[][] consumo = new int[10][12];
+        int[] sumaAnual = new int[10];
+        
+        viviendas = obtenerViviendas();
+        consumo = obtenerCosumo();
+        sumaAnual = obtenerSuma();
+        
+        obtenerReporte();
+        
+    }
+    public static int obtenerViviendas (int[] viviendan) {
+        
+        
     }
     
 }
