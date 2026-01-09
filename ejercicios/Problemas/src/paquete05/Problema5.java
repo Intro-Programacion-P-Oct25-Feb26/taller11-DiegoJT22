e0799c40285b8a51dcb5b6fde839c0cd469719a4
DiegoJT22 <daigoestrlar22@gmail.com>
2026-01-09 16:23:30 -0500

    solución Problema 5

--- ejercicios/Problemas/src/paquete05/Problema5.java
+++ ejercicios/Problemas/src/paquete05/Problema5.java
@@ -10,5 +10,38 @@ package paquete05;
  * @author reroes
  */
 public class Problema5 {
-    
+
+    public static void main(String[] args) {
+
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] sumaMatriz = obtenerSuma(informacion, informacion2);
+        String reporte = "";
+
+        for(int i = 0; i < sumaMatriz.length; i++) {
+            for (int j = 0; j < sumaMatriz[i].length; j++) {
+                reporte = String.format("%s%d\t", reporte, sumaMatriz[i][j]);
+            }
+            reporte = String.format("%s\n", reporte);
+        }
+
+        System.out.println(reporte);
+
+    }
+
+    public static int[][] obtenerSuma(int[][] a, int[][] b) {
+
+        int suma[][] = new int[3][3];
+        
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                suma[i][j] = a[i][j] + b[i][j];
+
+            }
+
+        }
+        return suma;
+
+    }
+
 }
