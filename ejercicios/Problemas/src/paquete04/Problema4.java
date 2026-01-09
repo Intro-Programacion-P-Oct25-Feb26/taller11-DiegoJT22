cfcc2493777a79b7989064d119231aca36549b30
DiegoJT22 <daigoestrlar22@gmail.com>
2026-01-09 13:32:14 -0500

    solución Problema 4

--- ejercicios/Problemas/src/paquete04/Problema4.java
+++ ejercicios/Problemas/src/paquete04/Problema4.java
@@ -5,6 +5,9 @@
  */
 package paquete04;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -15,7 +18,76 @@ public class Problema4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        
+        int respuestau;
+        String nombre;
+        String cedula;
+
+        System.out.printf("Ingrese el tipo de calculo que desea hacer "
+                + "( 1 Valor de Luz | 2 Calcular Predio )\n");
+        respuestau = entrada.nextInt();
+        
+        entrada.nextLine();
+
+        switch (respuestau) {
+            case 1:
+            case 2:
+                System.out.printf("Ingrese su nombre: \n");
+                nombre = entrada.nextLine();
+                System.out.printf("Ingrese su cedula: \n");
+                cedula = entrada.nextLine();
+                if (respuestau == 1) {
+                    obtenerValorLuz(nombre, cedula);
+                } else {
+                    obtenerCalcularPredio(nombre, cedula);
+                }
+
+                break;
+            default:
+                System.out.printf("Error Opcion no valida\n");
+
+        }       
     }
     
+    public static void obtenerValorLuz(String n, String c) {
+        
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);        
+        
+        double vKilowatio;
+        double nKilowatio;
+        double vLuz;
+        
+        System.out.printf("Ingrese el valor del kilowatio:\n");
+        vKilowatio = entrada.nextDouble();
+        System.out.printf("Ingrese el numero de kilowatios del mes:\n");
+        nKilowatio = entrada.nextDouble();
+        
+        vLuz = vKilowatio * nKilowatio;
+        
+        System.out.printf("Cliente %s con cedula %s debe canceclar el valor de %.2f\n",n,c,vLuz);
+    }
+    public static void obtenerCalcularPredio(String n, String c) {
+        
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US); 
+        
+        double porcentaje = 0.02;
+        double predio;
+        double vInmueble;
+        
+        System.out.printf("Ingrese el valor del Inmueble:\n");
+        
+        vInmueble = entrada.nextDouble();
+        
+        predio = vInmueble * porcentaje;
+        
+        System.out.printf("%s con cedula %s tiene un inmueble valorado en %.2f y"
+                + " tiene que pagar %.2f\n",n,c,vInmueble,predio);
+        
+    }
+
 }
