b97688fc3e8c3f9f8f9c9fd6a9160a3bda640417
DiegoJT22 <daigoestrlar22@gmail.com>
2026-01-09 17:17:52 -0500

    solución Problema 7

--- ejercicios/Problemas/src/paquete07/Problema7.java
+++ ejercicios/Problemas/src/paquete07/Problema7.java
@@ -9,6 +9,44 @@ package paquete07;
  *
  * @author reroes
  */
+import java.util.Scanner;
+
 public class Problema7 {
-    
+
+    public static void main(String[] args) {
+        String ciudades[] = arreglo();
+        presentar(ciudades);
+    }
+
+    public static String[] arreglo() {
+        Scanner entrada = new Scanner(System.in);
+        String ciudad;
+        System.out.println("Tamaño del arreglo:");
+        int indice = entrada.nextInt();
+        entrada.nextLine();
+        String ciudades[] = new String[indice];
+        for (int i = 0; i < ciudades.length; i++) {
+            System.out.printf("Nombre de la ciudad en la posicion %s :\n", i);
+            ciudad = entrada.nextLine();
+            ciudades[i] = ciudad;
+        }
+        return ciudades;
+    }
+
+    public static void presentar(String a[]) {
+        String mensaje = "";
+        String cadena;
+        int c = 0;
+        for (int i = 0; i < a.length; i++) {
+            cadena = a[i];
+            if (cadena.length() == 4 || cadena.length() == 5) {
+                c = c + 1;
+                mensaje = String.format("%s\t%s", mensaje, cadena);
+            } else if (c == 0) {
+                mensaje = "No hay valores que cumplan con el numero de caracteres";
+            }
+        }
+        System.out.printf("\n%s\n", mensaje);
+    }
+
 }
